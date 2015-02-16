efe1dbb905103da5a209a9369d7e7a9421f70853
Kevin Regner <krw10108@saintjoe.edu>
2015-02-16 11:19:08 -0600

    Update Fruit.js

--- Fruit.js
+++ Fruit.js
@@ -1,6 +1,8 @@
-var Fruit_List = "[/n " + [
+var fruit = [
 '{"Name" : "Apple", "Size of the fruit?" : "Medium and round.", "Tastes?" : "Good."}',
 	'{"Name" : "Pear", "Size of the fruit?" : "Medium and round.", "Tastes?" : "Good."}',
 	'{"Name" : "Grape", "Size of the fruit?" : "Small and round", "Tastes?" : "Good."}',
 	'{"Name" : "Banana", "Size of the fruit?" : "Long  and Curved.", "Tastes?" : "Good."}',
-	'{"Name" : "Grapefruit", "Size of the fruit?" : "Large and round.", "Tastes?" : "Good."}',
+	'{"Name" : "Grapefruit", "Size of the fruit?" : "Large and round.", "Tastes?" : "Good."}];
+	
+	print(fruit);
