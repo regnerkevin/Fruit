415aa161ffec84ae60ddb81feec52a77d385de94
Kevin Michael Regner <kregner@kregner.saintjoe.edu>
2015-02-16 12:16:53 -0500

    Fruit list

---
+++ Fruit.js
@@ -0,0 +1,6 @@
+var Fruit_List = "[/n " + [
+'{"Name" : "Apple", "Size of the fruit?" : "Medium and round.", "Tastes?" : "Good."}',
+	'{"Name" : "Pear", "Size of the fruit?" : "Medium and round.", "Tastes?" : "Good."}',
+	'{"Name" : "Grape", "Size of the fruit?" : "Small and round", "Tastes?" : "Good."}',
+	'{"Name" : "Banana", "Size of the fruit?" : "Long  and Curved.", "Tastes?" : "Good."}',
+	'{"Name" : "Grapefruit", "Size of the fruit?" : "Large and round.", "Tastes?" : "Good."}',
